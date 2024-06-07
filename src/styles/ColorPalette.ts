17b14c8a64083fc130d2559b5dcd09791d1c7634
porkieeeeeee <porkieeeeeee@gmail.com>
2024-06-07 15:58:17 +0900

    init::color palette 세팅

---
+++ src/styles/ColorPalette.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    grayWhite: "#fff",
+    gray50: "#EDF2F5",
+    gray400: "#919BA7",
+    gray800: "#4B535C",
+    gray900: "#3B4147",
+    grayBlack: "#111",
+    secondary300: "#D75C84",
+    secondary400: "#BC3B65",
+};
